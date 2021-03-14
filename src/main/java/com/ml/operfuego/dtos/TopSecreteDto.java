7944994e7fa50cbc9077b219c930c74000d57743
ezequiroga <ae.quiroga.88@gmail.com>
2021-03-14 10:45:16 -0300

    Se crea topsecrete dto

--- src/main/java/com/ml/operfuego/dtos/TopSecreteDto.java
+++ src/main/java/com/ml/operfuego/dtos/TopSecreteDto.java
@@ -1,11 +1,16 @@
 package com.ml.operfuego.dtos;
 
+import java.awt.Point;
+import lombok.Data;
+
 /**
  * DTO que modela la respuesta ultra secreta con la posicion y el mensaje
  * enemigo.
  *
  * @author ae_qu
  */
+@Data
 public class TopSecreteDto {
-    
+    private Point position;
+    private String message;
 }
