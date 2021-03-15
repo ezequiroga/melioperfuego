dd6f795790fba7de177e6810ef7eb9a1a56da506
ezequiroga <ae.quiroga.88@gmail.com>
2021-03-15 00:47:35 -0300

    Se crean los servicio de split, refactor de controllers CQRS

--- src/main/java/com/ml/operfuego/services/CentralDeInteligenciaService.java
+++ src/main/java/com/ml/operfuego/services/CentralDeInteligenciaService.java
@@ -1,7 +1,14 @@
 package com.ml.operfuego.services;
 
+import com.ml.operfuego.dtos.CoordenadaDto;
 import com.ml.operfuego.dtos.SateliteDto;
+import com.ml.operfuego.dtos.SatellitesDto;
+import com.ml.operfuego.dtos.TopSecreteDto;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,15 +18,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CentralDeInteligenciaService {
-    
+
+    @Autowired
+    private InteligenciaDePosicionesService inteligenciaDePosicionesService;
+    @Autowired
+    private InteligenciaDeMensajesService inteligenciaDeMensajeService;
+
     HashMap<String, SateliteDto> satelites = new HashMap<>();
-    
-    public int getCantidadDeSatelites(){
+
+    public int getCantidadDeSatelites() {
         return satelites.size();
     }
     
-    public void registrarSatelite(SateliteDto satelite){
-        satelites.put(satelite.getName(), satelite);
+    public boolean sateliteRegistrado(String name){
+        return satelites.containsKey(name);
     }
-    
+
+    public void registrarSatelite(SateliteDto satelite) {
+        if (!satelites.containsKey(satelite.getName())) {
+            satelites.put(satelite.getName(), satelite);
+        } else {
+            satelites.replace(satelite.getName(), satelite);
+        }
+    }
+
+    public TopSecreteDto informacionUltraSecreta(SatellitesDto satellitesDto) {
+        CoordenadaDto coordenadas = inteligenciaDePosicionesService.posisionNaveEnemiga(satellitesDto);
+        String mensaje = inteligenciaDeMensajeService.reconstruirMensaje(satellitesDto);
+        return new TopSecreteDto(coordenadas, mensaje);
+    }
+
+    public TopSecreteDto informacionUltraSecreta() {
+        SatellitesDto satellitesDto = new SatellitesDto();
+        List<SateliteDto> satelitesList = new LinkedList<>();
+        for (Map.Entry<String, SateliteDto> entry : satelites.entrySet()) {
+            satelitesList.add(entry.getValue());
+        }
+        satellitesDto.setSatellites(satelitesList);
+        return informacionUltraSecreta(satellitesDto);
+    }
+
 }
