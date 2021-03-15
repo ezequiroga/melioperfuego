9714f9799fc8db34a84e948dc207b95af7846b86
ezequiroga <ae.quiroga.88@gmail.com>
2021-03-14 21:03:39 -0300

    Se agrega test del servicio para calcular la posicion de la nave enemiga

---
+++ src/test/java/com/ml/operfuego/services/InteligenciaDePosicionesServiceTest.java
@@ -0,0 +1,45 @@
+package com.ml.operfuego.services;
+
+import com.ml.operfuego.dtos.CoordenadaDto;
+import com.ml.operfuego.dtos.SateliteDto;
+import com.ml.operfuego.dtos.SatellitesDto;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ae_qu
+ */
+public class InteligenciaDePosicionesServiceTest {
+    
+    public InteligenciaDePosicionesServiceTest() {
+    }
+
+    @Test
+    public void testPosisionNaveEnemiga() {
+        System.out.println("posisionNaveEnemiga");
+        
+        SateliteDto sX = new SateliteDto("sX", Math.sqrt(26), null, new CoordenadaDto(2, 6));
+        SateliteDto sY = new SateliteDto("sY", Math.sqrt(25), null, new CoordenadaDto(5, 4));
+        SateliteDto sZ = new SateliteDto("sZ", Math.sqrt(40), null, new CoordenadaDto(7, 3));
+        
+        List<SateliteDto> satelites = new LinkedList<>();
+        satelites.add(sX);
+        satelites.add(sY);
+        satelites.add(sZ);
+        
+        SatellitesDto satellitesDto = new SatellitesDto();
+        satellitesDto.setSatellites(satelites);
+        
+        InteligenciaDePosicionesService instance = new InteligenciaDePosicionesService();
+        
+        CoordenadaDto expResult = new CoordenadaDto(1.0, 1.0);
+        CoordenadaDto resultAux = instance.posisionNaveEnemiga(satellitesDto);
+        CoordenadaDto result = new CoordenadaDto(Math.ceil(resultAux.x), Math.ceil(resultAux.y));
+        
+        assertEquals(expResult, result);
+    }
+    
+}
